27f2590b0f19f0096949a39ed65601e71d75ddec
Maksim Fotin <maksimfotin144@gmail.com>
2024-03-16 19:22:59 +0700

    fix product, category, test_category, test_product

--- category.py
+++ category.py
@@ -1,12 +1,16 @@
 class Category:
-    name: str
-    description: str
-    products: list
-    category_count = 0
-    total_categories = 0
+    """
+    Класс для категорий товара
+    """
 
-    def __init__(self, name, description):
-        self.name = name  # название категории (строка)
-        self.description = description  # описание категории (строка)
-        self.products = []  # список товаров в данной категории
-        Category.total_categories += 1
+    category_count = 0
+    product_count = 0
+
+    def __init__(
+            self, name: str, description: str, products: list = None
+    ) -> None:
+        self.name = name
+        self.description = description
+        self.products = products
+        Category.category_count += 1
+        Category.product_count += len(self.products)
